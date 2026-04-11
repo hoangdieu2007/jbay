7181f0b549752f50e5fb20630bbcf6eaffaa416d
Hoang Xuan Dieu <25021672@vnu.edu.vn>
2026-04-11 15:08:16 +0700

    strip down entity

--- src/main/java/a88/jbay/model/entity/Entity.java
+++ src/main/java/a88/jbay/model/entity/Entity.java
@@ -4,26 +4,11 @@ import java.time.LocalDateTime;
 
 public abstract class Entity {
     protected String id;
-    protected final LocalDateTime created;
-    protected LocalDateTime modified;
-    protected boolean deleted;
 
     public Entity() {
-        this.created = LocalDateTime.now();
-        this.modified = this.created;
-        this.deleted = false;
     }
 
     public String getId() {
         return id;
-    }
-    public LocalDateTime getCreated() {
-        return created;
-    }
-    public LocalDateTime getModified() {
-        return modified;
-    }
-    public boolean isDeleted() {
-        return deleted;
     }
 }
