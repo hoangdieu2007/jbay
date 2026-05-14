7daf6679c2c6ede92a8f54bf7317e30bb507f3ed
Hoang Xuan Dieu <25021672@vnu.edu.vn>
2026-05-14 16:52:14 +0700

    trying to fix the network deadlock

--- src/main/java/a88/jbay/server/ClientConnection.java
+++ src/main/java/a88/jbay/server/ClientConnection.java
@@ -113,11 +113,9 @@ public class ClientConnection implements Runnable {
 
         try {
             // Use atomic write to prevent deadlocks
-            synchronized(out) {
-                out.writeObject(response);
-                out.flush();
-                out.reset();
-            }
+            out.writeObject(response);
+            out.flush();
+            out.reset();
         } catch (IOException e) {
             logger.error("Error sending response: " + e.getMessage(), e);
         }
