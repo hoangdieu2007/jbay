05a6fe3cc2150ddb39413ee551bbf1e045c83ab8
khangtran <duckhang12b@gmail.com>
2026-05-20 02:53:44 +0700

    update new UI for userHomescree

---
+++ src/main/java/a88/jbay/controller/client/OngoingAuctionsController.java
@@ -0,0 +1,7 @@
+package a88.jbay.controller.client;
+
+import javafx.fxml.FXML;
+
+public class OngoingAuctionsController {
+
+}
---
+++ src/main/java/a88/jbay/controller/client/UserHomeScreenController.java
@@ -0,0 +1,26 @@
+package a88.jbay.controller.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class UserHomeScreenController {
+
+    public void initialize(){
+
+    }
+
+    @FXML
+    private void handleLogOut(){
+
+    }
+
+    @FXML
+    private void showMyListings(){
+
+    }
+
+    @FXML
+    private void showOngoingAuctions(){
+
+    }
+}
