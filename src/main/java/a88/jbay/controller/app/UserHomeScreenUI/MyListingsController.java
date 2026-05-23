2c76de6755d5acc3d35755ef32f5ade42c651c31
khangtran <duckhang12b@gmail.com>
2026-05-24 01:39:43 +0700

    update controller to save memory

--- src/main/java/a88/jbay/controller/app/UserHomeScreenUI/MyListingsController.java
+++ src/main/java/a88/jbay/controller/app/UserHomeScreenUI/MyListingsController.java
@@ -14,6 +14,8 @@ import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyListingsController {
 
@@ -24,41 +26,56 @@ public class MyListingsController {
     @FXML
     private TilePane sellerTilePane;
 
-    private ObservableMap<Integer, Auction> sellerMap = ClientSession.getInstance().getSellerAuctions();
-
     private ObservableList<Auction> sellerList = FXCollections.observableArrayList();
+
+    Map<Integer, VBox> sellerCard = new HashMap<>();
+    Map<Integer, SellerItemCardController> controllerMap = new HashMap<>();
 
     @FXML
     private void initialize(){
         sellerTilePane.getChildren().clear();
 
-        sellerList.setAll(sellerMap.values());
+        ObservableMap<Integer, Auction> sellerMap = ClientSession.getInstance().getSellerAuctions();
 
         FilteredList<Auction> filteredList = new FilteredList<>(sellerList, auction -> true);
+
+        filteredList.addListener((ListChangeListener<Auction>) change ->{
+            refreshSellerList(filteredList);
+        });
+
+        for (Auction auction : sellerMap.values()){
+            if(!sellerCard.containsKey(auction.getId())){
+                sellerList.add(auction);
+                createCardSeller(auction);
+            }
+        }
 
         sellerMap.addListener((MapChangeListener<Integer, Auction>) change -> {
 
             if(change.wasAdded() && change.wasRemoved()){
                 int idx = sellerList.indexOf(change.getValueRemoved()); // if cant find auction --> return -1
-
+                SellerItemCardController controller = controllerMap.get(change.getKey());
+                if(controller != null) {
+                    controller.setData(change.getValueAdded());
+                }
                 if(idx >= 0){
                     sellerList.set(idx, change.getValueAdded()); // if idx = -1 --> crash
                 }
             }
             else if (change.wasAdded()){
                 sellerList.add(change.getValueAdded());
+                createCardSeller(change.getValueAdded());
             }
             else if(change.wasRemoved()){
                 sellerList.remove(change.getValueRemoved());
+                sellerCard.remove(change.getKey());
+                controllerMap.remove(change.getKey());
             }
 
             sellerList.sort(Comparator.comparingInt(Auction :: getId).reversed());
 
         } );
 
-        filteredList.addListener((ListChangeListener<Auction>) change ->{
-            refreshSellerList(filteredList);
-        });
 
         sellerSearchField.textProperty().addListener((observable, oldValue, newValue) -> {
             filteredList.setPredicate(auction -> {
@@ -86,6 +103,9 @@ public class MyListingsController {
             SellerItemCardController controller = loader.getController();
             controller.setData(auction);
 
+            sellerCard.put(auction.getId(), cardBox);
+            controllerMap.put(auction.getId(), controller);
+
             return cardBox;
         } catch (IOException e) {
             return null;
@@ -96,9 +116,12 @@ public class MyListingsController {
     public void refreshSellerList(FilteredList<Auction> filteredList) {
         sellerTilePane.getChildren().clear();
         for (Auction auction : filteredList) {
-            VBox card = createCardSeller(auction);
-            if (card != null) {
-                sellerTilePane.getChildren().add(card);
+            int id = auction.getId();
+            if (sellerCard.containsKey(id)) {
+                VBox card = sellerCard.get(id);
+                if (card != null) {
+                    sellerTilePane.getChildren().add(card);
+                }
             }
         }
     }
