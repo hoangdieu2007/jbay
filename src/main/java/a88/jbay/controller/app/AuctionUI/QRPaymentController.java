9c6a0b8b29fc75815a199de1a3a5d8a403cdc90c
HoangDuck215 <hoangduc05122007a5@gmail.com>
2026-05-26 11:17:04 +0700

    fix something for qr controller

--- src/main/java/a88/jbay/controller/app/AuctionUI/QRPaymentController.java
+++ src/main/java/a88/jbay/controller/app/AuctionUI/QRPaymentController.java
@@ -1,7 +1,6 @@
 package a88.jbay.controller.app.AuctionUI;
 
 import a88.jbay.common.auction.Auction;
-import a88.jbay.controller.ControllerProvider;
 import a88.jbay.util.ImageProcessor;
 import a88.jbay.view.ViewManager;
 import javafx.fxml.FXML;
@@ -17,14 +16,13 @@ public class QRPaymentController {
     @FXML
     private ImageView qrImageView;
 
-    // Biến tạm để nhớ món hàng cần quay về
     private Auction auctionContext;
 
     /**
      * Hàm nạp dữ liệu từ ResponseHandler hoặc Controller khác
      * @param qrCode Mảng byte của ảnh QR nhận từ Server
      * @param sellerName Tên người bán
-     * @param auction Đối tượng Auction hiện tại để tí nữa quay lại
+     * @param auction Đối tượng Auction hiện tại
      */
     public void setData(byte[] qrCode, String sellerName, Auction auction) {
         this.auctionContext = auction;
@@ -43,17 +41,8 @@ public class QRPaymentController {
     @FXML
     private void handleClose() {
         try {
-            // 1. Load lại màn hình chi tiết món hàng dành cho người mua (Bidder)
-            ViewManager.getInstance().loadIntoMainScene("AuctionUI/client-bidder-item-view.fxml");
-
-            // 2. Lấy Controller vừa được khởi tạo ra
-            ClientBidderItemController controller = ControllerProvider.getInstance()
-                    .getController(ClientBidderItemController.class);
-
-            // 3. Đổ lại dữ liệu cũ vào để người dùng tiếp tục xem món hàng đó
-            if (controller != null && auctionContext != null) {
-                controller.setCurrentAuction(auctionContext);
-            }
+            // Load thẳng lại màn hình danh sách Ongoing Auctions của trang chủ
+            ViewManager.getInstance().loadIntoMainScene("UserHomeScreenUI/ongoing-Auctions.fxml");
         } catch (IOException e) {
             e.printStackTrace();
         }
